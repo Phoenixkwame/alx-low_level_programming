8b6f4ec6764730eb58e2679da511bbcc232dbf8a
Jude Ofosu <ofosujude111@gmail.com>
2022-04-07 20:46:53 -0700

    This is commit for 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzel");
+return (0);
+}
