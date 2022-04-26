79032692f340126ce990db3925a366f75789633f
Jude Ofosu <93232739+Phoenixkwame@users.noreply.github.com>
2022-04-26 11:32:39 +0000

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - Entry point and function name
+*
+* @a: pointer name
+*
+* @size: variable name
+*
+* This function prints the sum of two diagonals of a square matrix
+*
+* Return: nothing
+**/
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
